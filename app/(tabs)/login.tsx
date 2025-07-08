3085e2d8850cad3d7f6f71b4d0ed9c2b5e99df9d
Kavinkumar <kavinkumar@Kavinkumars-Mac-mini.local>
2025-07-09 00:13:46 +0530

    InitialRoute

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export default function LoginScreen(){
+        return(
+            <View>
+                <Text>Login Screen by Kavin</Text>
+            </View>
+        );
+}
